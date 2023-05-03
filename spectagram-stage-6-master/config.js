0a2c9939454025cdffdc4643ad490076846aae28
Divyam <129356833+Furyyyyyyyyy@users.noreply.github.com>
2023-05-03 10:43:51 +0530

    Add files via upload

---
+++ spectagram-stage-6-master/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAeFBbaswAP6Xb1uT1F6POXUpO8Z3sVDM8",
+  authDomain: "spectagram-40bea.firebaseapp.com",
+  databaseURL: "https://spectagram-40bea-default-rtdb.firebaseio.com",
+  projectId: "spectagram-40bea",
+  storageBucket: "spectagram-40bea.appspot.com",
+  messagingSenderId: "244252592505",
+  appId: "1:244252592505:web:6691eb9ad3f77041f487a8"
+};
